424277fd18f21d4bd4177f1a4f04fe33fa964c2b
Ben Honken <honkenb@gmail.com>
2020-02-11 14:09:23 -0600

    added text to inform user which fields they can search by.

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,7 @@ function SearchForm(props) {
   return (
     <div className="card text-center">
       <div className="card-header">
-        <h2>{props.searchtype} Search</h2>
+        <h2>Search by name, email, city, or state!</h2>
       </div>
       <div className="card-body"></div>
       <form>
@@ -17,7 +17,7 @@ function SearchForm(props) {
             name="search"
             type="text"
             className="form-control"
-            placeholder="Search Cars"
+            placeholder="Search Employees"
             id="search"
           />
           <br />
